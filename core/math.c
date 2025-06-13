b462ccc5f9db63c14a680f696e04619576c6b686
Anton <singaraiona@gmail.com>
2025-06-13 14:21:07 +0200

    feat(math): reuse arguments as out if rc == 1

--- core/math.c
+++ core/math.c
@@ -1480,7 +1480,7 @@ obj_p unop_map(raw_p op, obj_p x) {
 
     pool = runtime_get()->pool;
     n = pool_split_by(pool, l, 0);
-    out = vector(x->type, l);
+    out = (rc_obj(x) == 1) ? clone_obj(x) : vector(x->type, l);
 
     if (n == 1) {
         argv[0] = (raw_p)x;
@@ -1543,7 +1543,7 @@ obj_p binop_map(raw_p op, obj_p x, obj_p y) {
 
     pool = runtime_get()->pool;
     n = pool_split_by(pool, l, 0);
-    out = vector(t, l);
+    out = (rc_obj(x) == 1) ? clone_obj(x) : (rc_obj(y) == 1) ? clone_obj(y) : vector(t, l);
 
     if (n == 1) {
         argv[0] = (raw_p)x;
